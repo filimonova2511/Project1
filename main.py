f0ed9894f0cc073ac32c497f21b89e4164aeef6d
Yengeniya Filimonova <filimonova25-11@mail.ru>
2022-05-30 13:27:51 +0300

    add dialog

--- main.py
+++ main.py
@@ -2,4 +2,9 @@ print('hello world!!!!!!!!!!!!!!!!')
 name = input('Имя - ')
 print('Hi, ', name, '!')
 print("How are you?")
-answer = input('Good/bad')
+answer = input('Good/bad - ')
+if answer == 'good':
+    print('Fine! See you later!')
+else:
+    print('Can I help you?')
+
